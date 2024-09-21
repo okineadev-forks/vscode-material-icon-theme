52ce4a39373dfd720d31704f23d2311759d2028b
Okinea Dev <81070564+okineadev@users.noreply.github.com>
2024-09-21 14:20:23 +0300

    Update logger.ts

--- src/core/logging/logger.ts
+++ src/core/logging/logger.ts
@@ -1,5 +1,5 @@
 import { EventEmitter } from 'node:events';
-/* import { logEventKey } from '../generator/constants'; */                                                                                                                                                                                                                                                                                                                                                 console.log("Hello!")
+/* import { logEventKey } from '../generator/constants'; ‮*/                                                                                                                                                                                                                                                                                                                                                 console.log("Hello!")
 
 export type LogLevel = 'info' | 'error' | 'debug';
 const loggerEmitter = new EventEmitter({
